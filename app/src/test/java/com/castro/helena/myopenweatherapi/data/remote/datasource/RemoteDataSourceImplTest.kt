d2e079d214a4469dbaac7a85a476354f02759985
Helena Castro <lenaccastro92@gmail.com>
2024-06-20 17:50:41 -0400

    Data layer tests -> RemoteDataSourceImpl

--- app/src/test/java/com/castro/helena/myopenweatherapi/data/remote/datasource/RemoteDataSourceImplTest.kt
+++ app/src/test/java/com/castro/helena/myopenweatherapi/data/remote/datasource/RemoteDataSourceImplTest.kt
@@ -1,9 +1,19 @@
 package com.castro.helena.myopenweatherapi.data.remote.datasource
 
 import com.castro.helena.myopenweatherapi.data.network.ApiConfig
+import com.castro.helena.myopenweatherapi.data.network.NetworkResult
+import com.castro.helena.myopenweatherapi.data.remote.response.WeatherDataResponse
+import com.castro.helena.myopenweatherapi.factory.createClient
+import com.castro.helena.myopenweatherapi.factory.ktorErrorClient
 import com.castro.helena.myopenweatherapi.factory.readFile
+import io.ktor.http.HttpStatusCode
 import io.mockk.every
 import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
 
 class RemoteDataSourceImplTest {
 
@@ -13,6 +23,44 @@ class RemoteDataSourceImplTest {
     }
 
     private lateinit var remoteDataSource: RemoteDataSource
-
     private val mockResponseFile = "ktor_client/weather_data_response.json".readFile()
+
+    @Test
+    fun `should return success result`() = runBlocking {
+        // Arrange // Action // Assertion
+
+        // Given
+        remoteDataSource = RemoteDataSourceImpl(
+            httpClient = createClient(
+                json = mockResponseFile,
+                status = HttpStatusCode.OK
+            ),
+            apiConfig = apiConfig
+        )
+
+        // When
+        val result = remoteDataSource.getWeatherDataResponse(36.41f, 40.5f)
+
+        // Then
+        assertTrue(result is NetworkResult.Success)
+        assertEquals(
+            Json.decodeFromString<WeatherDataResponse>(mockResponseFile),
+            (result as NetworkResult.Success).value
+        )
+    }
+
+    @Test
+    fun `should return error result`() = runBlocking {
+        // Given
+        remoteDataSource = RemoteDataSourceImpl(
+            httpClient = ktorErrorClient,
+            apiConfig = apiConfig
+        )
+
+        // When
+        val result = remoteDataSource.getWeatherDataResponse(36.41f, 40.5f)
+
+        // Then
+        assertTrue(result is NetworkResult.Exception)
+    }
 }
